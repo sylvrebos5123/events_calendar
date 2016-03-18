e95f7587cd6d15048e2a6ae62b663f53b00c7a5e
vrebos.sylvie <sylvievrebos@gmail.com>
2016-03-18 17:24:16 +0100

    adding modify functionnality

--- demos/php/add_event.php
+++ demos/php/add_event.php
@@ -1,10 +1,11 @@
 <?php
-
-if(isset($_POST))
+var_dump($_GET);
+if(isset($_GET))
 {
-    foreach($_POST as $k=>$v)
+
+    foreach($_GET as $k=>$v)
     {
-        $_POST[$k]=htmlspecialchars(strip_tags($_POST[$k]));
+        $_GET[$k]=htmlspecialchars($_GET[$k]);
     }
 }
 else
@@ -13,14 +14,24 @@ else
 }
 
 require dirname(__FILE__) . '/connect_db.php';
-
-
+echo $_GET['id_event'];
+if($_GET['id_event']==0)
+{
 $query = $db->prepare("insert INTO events(event_title,event_place,start_date_time,end_date_time,status)
                                   values (:event_title,:event_place,:start_date_time,:end_date_time,1);");
-
-$query->bindValue(':event_title', $_POST['event_title'], PDO::PARAM_STR);
-$query->bindValue(':event_place', $_POST['event_place'], PDO::PARAM_STR);
-$query->bindValue(':start_date_time', $_POST['start_date_time'], PDO::PARAM_STR);
-$query->bindValue(':end_date_time', $_POST['end_date_time'], PDO::PARAM_STR);
+}
+else
+{
+    $query = $db->prepare("update from events set event_title=:event_title,event_place=:event_place,
+                                                  start_date_time=:start_date_time,end_date_time::end_date_time
+                                                  where id_event=:id_event ;");
+}
+$query->bindValue(':id_event', $_GET['id_event'], PDO::PARAM_INT);
+$query->bindValue(':event_title', $_GET['event_title'], PDO::PARAM_STR);
+$query->bindValue(':event_place', $_GET['event_place'], PDO::PARAM_STR);
+$query->bindValue(':start_date_time', $_GET['start_date_time'], PDO::PARAM_STR);
+$query->bindValue(':end_date_time', $_GET['end_date_time'], PDO::PARAM_STR);
 
 $query->execute();
+
+//var_dump($query);
