1ca995da6a1c726af4fd43fe384a5e9a8b0c1615
vrebos.sylvie <sylvievrebos@gmail.com>
2016-06-16 17:10:21 +0200

    remplace symbole - by |

--- demos/php/file_test.php
+++ demos/php/file_test.php
@@ -15,7 +15,7 @@ $i=0;
 //print_r($array_events);
 foreach ($array_events as $k=>$v) {
     $output_arrays[$i]['id']=$v->id_event;
-    $output_arrays[$i]['title']=$v->event_title.' - '.$v->event_place;
+    $output_arrays[$i]['title']=$v->event_title.' | '.$v->event_place;
     //$output_arrays[$i]['start']=($v->start_time=='00:00:00')?$v->start_date : $v->start_date.'T'.$v->start_time;
     //$output_arrays[$i]['end']=($v->end_time=='00:00:00')?$v->end_date : $v->end_date.'T'.$v->end_time;
     $output_arrays[$i]['start']=$v->start_date_time;
